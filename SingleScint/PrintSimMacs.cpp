953eb6926a2caeceab680bcdfc208cce2de5734e
Gry M. Tveten <g.m.tveten@fys.uio.no>
2016-01-07 12:36:34 +0100

    Forgot a little detail ... telling what particle I wanted. The line with particle definition is back in place.

--- SingleScint/PrintSimMacs.cpp
+++ SingleScint/PrintSimMacs.cpp
@@ -1,7 +1,3 @@
-
-
-//#include <iostream>
-//#include <fstream>
 /***************** Written by Gry in december 2015 *******************
 ****************** changes made in january 2016
 This file generates a macros for runnin GEANT4 simulations with
@@ -15,7 +11,7 @@ To run all the simulations use the script called manyruns.sh like this
 source manyruns.sh
 ******************/
 int PrintSimMacs(int k=0){
-Int_t N = 500000; //Number of gammas to be simulated
+Int_t N = 5000000; //Number of gammas to be simulated
 Double_t mintheta = 160.; //smallest theta angle to be used for the gamma distribution [deg]
 Double_t maxtheta = 180.; //largest theta angle
 Double_t Egammalow = 500.; //Lowest gamma energy to be simulated
@@ -29,11 +25,14 @@ Egamma = Egammalow + k*Egammastep;
 
 macfile<<endl;
 macfile<<"## Particle type, position, energy..."<<endl;
+
+macfile<<"/gps/particle gamma"<<endl;
 macfile<<"/gps/number 1"<<endl;
 macfile<<"# Source position now hardcoded"<<endl;
 macfile<<endl;
 macfile<<"/gps/energy "<<Egamma<<" keV"<<endl;
 macfile<<endl;
+//macfile<<"/gps/direction 0 0 1"<<endl;
 macfile<<"/gps/ang/type iso"<<endl;
 macfile<<"/gps/ang/mintheta "<< mintheta <<" degree"<<endl;
 macfile<<"/gps/ang/maxtheta "<< maxtheta <<" degree"<<endl;
