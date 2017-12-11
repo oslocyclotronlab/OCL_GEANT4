oslocyclotronlab/OCL_GEANT4

commit f72c738d9332c5ae5e69bd4e8ec9c773d860c09e

Edited the ordering in the array of Crystals so that the copy number should match the actual detector numbering in the lab

SingleScint/src/SingleScintDetectorConstruction.cc
@@ -244,110 +244,201 @@ void SingleScintDetectorConstruction::SetPlacementParameters()
 
 G4double distColltoDet = 10*mm; // Distance between Collimator and Detector (surface to Surf)
 
-//Det. number 3 at OCL:
-OCLLaBr3_presence[ 0]		= true;             
-OCLCollimator_presence[ 0]	= true;             
-OCLLaBr3_Distance[ 0]		= 20*cm;             
-OCLLaBr3_theta[ 0]			= 142.622528*deg;             
-OCLLaBr3_phi[ 0]			= 144.000134*deg;
+//Beamline            
+OCLLaBr3_presence[0]		= false;             
+OCLCollimator_presence[0]	= false;             
+OCLLaBr3_Distance[0]		= 20*cm;             
+OCLLaBr3_theta[00]			= 180.000000*deg;             
+OCLLaBr3_phi[0]			= 180.000000*deg;
 
-//Det. number 4 at OCL             
+//Det. number 1 at OCL:            
 OCLLaBr3_presence[ 1]		= true;             
 OCLCollimator_presence[ 1]	= true;             
 OCLLaBr3_Distance[ 1]		= 20*cm;             
 OCLLaBr3_theta[ 1]			= 142.622561*deg;             
-OCLLaBr3_phi[ 1]			= 216.000153*deg;
-             
-//Det. number 5 at OCL  
+OCLLaBr3_phi[ 1]			= 359.999847*deg;
+
+//Det. number 2 at OCL             
 OCLLaBr3_presence[ 2]		= true;             
 OCLCollimator_presence[ 2]	= true;             
 OCLLaBr3_Distance[ 2]		= 20*cm;             
-OCLLaBr3_theta[ 2]			= 142.622535*deg;             
-OCLLaBr3_phi[ 2]			= 288.000000*deg;
+OCLLaBr3_theta[ 2]			= 142.622528*deg;             
+OCLLaBr3_phi[ 2]			= 71.999866*deg;
 
-//Det. number 1 at OCL:            
+//Det. number 3 at OCL:
 OCLLaBr3_presence[ 3]		= true;             
 OCLCollimator_presence[ 3]	= true;             
 OCLLaBr3_Distance[ 3]		= 20*cm;             
-OCLLaBr3_theta[ 3]			= 142.622561*deg;             
-OCLLaBr3_phi[ 3]			= 359.999847*deg;
+OCLLaBr3_theta[ 3]			= 142.622528*deg;             
+OCLLaBr3_phi[ 3]			= 144.000134*deg;
 
-//Det. number 2 at OCL             
+//Det. number 4 at OCL             
 OCLLaBr3_presence[ 4]		= true;             
 OCLCollimator_presence[ 4]	= true;             
 OCLLaBr3_Distance[ 4]		= 20*cm;             
-OCLLaBr3_theta[ 4]			= 142.622528*deg;             
-OCLLaBr3_phi[ 4]			= 71.999866*deg;
-
-//Det. number 11        
+OCLLaBr3_theta[ 4]			= 142.622561*deg;             
+OCLLaBr3_phi[ 4]			= 216.000153*deg;
+             
+//Det. number 5 at OCL  
 OCLLaBr3_presence[ 5]		= true;             
 OCLCollimator_presence[ 5]	= true;             
 OCLLaBr3_Distance[ 5]		= 20*cm;             
-OCLLaBr3_theta[ 5]			= 100.812191*deg;             
-OCLLaBr3_phi[ 5]			= 144.000046*deg;
+OCLLaBr3_theta[ 5]			= 142.622535*deg;             
+OCLLaBr3_phi[ 5]			= 288.000000*deg;
 
-//Det. number 22             
-OCLLaBr3_presence[ 6]		= true;             
-OCLCollimator_presence[ 6]	= true;             
-OCLLaBr3_Distance[ 6]		= 20*cm;             
-OCLLaBr3_theta[ 6]			= 79.187559*deg;             
-OCLLaBr3_phi[ 6]			= 180.000044*deg;
+//Det. number 6           
+OCLLaBr3_presence[6]		= true;             
+OCLCollimator_presence[6]	= true;             
+OCLLaBr3_Distance[6]		= 20*cm;             
+OCLLaBr3_theta[6]			= 116.564893*deg;             
+OCLLaBr3_phi[6]				= 323.999989*deg;
 
-//Det. number 13:             
+//Det. number 7
 OCLLaBr3_presence[ 7]		= true;             
 OCLCollimator_presence[ 7]	= true;             
 OCLLaBr3_Distance[ 7]		= 20*cm;             
 OCLLaBr3_theta[ 7]			= 100.812208*deg;             
-OCLLaBr3_phi[ 7]			= 215.999956*deg;
+OCLLaBr3_phi[ 7]			= 0.000044*deg;
 
-//Det. number 24             
+//Det. number 8           
 OCLLaBr3_presence[ 8]		= true;             
 OCLCollimator_presence[ 8]	= true;             
 OCLLaBr3_Distance[ 8]		= 20*cm;             
-OCLLaBr3_theta[ 8]			= 79.187575*deg;             
-OCLLaBr3_phi[ 8]			= 251.999954*deg;
+OCLLaBr3_theta[ 8]			= 116.564908*deg;             
+OCLLaBr3_phi[ 8]			= 35.999955*deg;
 
-//Det. number 15             
-OCLLaBr3_presence[ 9]		= false;             
-OCLCollimator_presence[ 9]	= false;             
+//Det. number 9 at OCL:             
+OCLLaBr3_presence[ 9]		= true;             
+OCLCollimator_presence[ 9]	= true;             
 OCLLaBr3_Distance[ 9]		= 20*cm;             
-OCLLaBr3_theta[ 9]			= 100.812175*deg;             
-OCLLaBr3_phi[ 9]			= 288.000000*deg;
- 
-//Det. number 16           
+OCLLaBr3_theta[ 9]			= 100.812191*deg;             
+OCLLaBr3_phi[ 9]			= 71.999954*deg;
+
+//Det. number 10             
 OCLLaBr3_presence[10]		= true;             
 OCLCollimator_presence[10]	= true;             
 OCLLaBr3_Distance[10]		= 20*cm;             
-OCLLaBr3_theta[10]			= 79.187575*deg;             
-OCLLaBr3_phi[10]			= 324.000046*deg;
-             
-//Det. number 7
+OCLLaBr3_theta[10]			= 116.564844*deg;             
+OCLLaBr3_phi[10]			= 108.000000*deg;
+
+//Det. number 11        
 OCLLaBr3_presence[11]		= true;             
 OCLCollimator_presence[11]	= true;             
 OCLLaBr3_Distance[11]		= 20*cm;             
-OCLLaBr3_theta[11]			= 100.812208*deg;             
-OCLLaBr3_phi[11]			= 0.000044*deg;
-    
-//Det. number 18      
+OCLLaBr3_theta[11]			= 100.812191*deg;             
+OCLLaBr3_phi[11]			= 144.000046*deg;
+
+//Det. number 12             
 OCLLaBr3_presence[12]		= true;             
 OCLCollimator_presence[12]	= true;             
 OCLLaBr3_Distance[12]		= 20*cm;             
-OCLLaBr3_theta[12]			= 79.187559*deg;             
-OCLLaBr3_phi[12]			= 35.999956*deg;
+OCLLaBr3_theta[12]			= 116.564908*deg;             
+OCLLaBr3_phi[12]			= 180.000045*deg;
 
-//Det. number 9 at OCL:             
+//Det. number 13:             
 OCLLaBr3_presence[13]		= true;             
 OCLCollimator_presence[13]	= true;             
 OCLLaBr3_Distance[13]		= 20*cm;             
-OCLLaBr3_theta[13]			= 100.812191*deg;             
-OCLLaBr3_phi[13]			= 71.999954*deg;
+OCLLaBr3_theta[13]			= 100.812208*deg;             
+OCLLaBr3_phi[13]			= 215.999956*deg;
 
-//Det. number 20             
+//Det. number 14             
 OCLLaBr3_presence[14]		= true;             
 OCLCollimator_presence[14]	= true;             
 OCLLaBr3_Distance[14]		= 20*cm;             
-OCLLaBr3_theta[14]			= 79.187591*deg;             
-OCLLaBr3_phi[14]			= 108.000000*deg;
+OCLLaBr3_theta[14]			= 116.564893*deg;             
+OCLLaBr3_phi[14]			= 252.000011*deg;
+
+//Det. number 15             
+OCLLaBr3_presence[15]		= true;             
+OCLCollimator_presence[15]	= true;             
+OCLLaBr3_Distance[15]		= 20*cm;             
+OCLLaBr3_theta[15]			= 100.812175*deg;             
+OCLLaBr3_phi[15]			= 288.000000*deg;
+
+//Det. number 16           
+OCLLaBr3_presence[16]		= true;             
+OCLCollimator_presence[16]	= true;             
+OCLLaBr3_Distance[16]		= 20*cm;             
+OCLLaBr3_theta[16]			= 79.187575*deg;             
+OCLLaBr3_phi[16]			= 324.000046*deg;
+
+//Det. number 17             
+OCLLaBr3_presence[17]		= true;             
+OCLCollimator_presence[17]	= true;             
+OCLLaBr3_Distance[17]		= 20*cm;             
+OCLLaBr3_theta[17]			= 63.434885*deg;             
+OCLLaBr3_phi[17]			= 0.000045*deg;
+
+//Det. number 18      
+OCLLaBr3_presence[18]		= true;             
+OCLCollimator_presence[18]	= true;             
+OCLLaBr3_Distance[18]		= 20*cm;             
+OCLLaBr3_theta[18]			= 79.187559*deg;             
+OCLLaBr3_phi[18]			= 35.999956*deg;
+
+//Det. number 19             
+OCLLaBr3_presence[19]		= true;             
+OCLCollimator_presence[19]	= true;             
+OCLLaBr3_Distance[19]		= 20*cm;             
+OCLLaBr3_theta[19]			= 63.434900*deg;             
+OCLLaBr3_phi[19]			= 72.000011*deg;
+
+//Det. number 20             
+OCLLaBr3_presence[20]		= true;             
+OCLCollimator_presence[20]	= true;             
+OCLLaBr3_Distance[20]		= 20*cm;             
+OCLLaBr3_theta[20]			= 79.187591*deg;             
+OCLLaBr3_phi[20]			= 108.000000*deg;
+
+//Det. number 21             
+OCLLaBr3_presence[21]		= true;             
+OCLCollimator_presence[21]	= true;             
+OCLLaBr3_Distance[21]		= 20*cm;             
+OCLLaBr3_theta[21]			= 63.434900*deg;             
+OCLLaBr3_phi[21]			= 143.999989*deg;
+
+//Det. number 22             
+OCLLaBr3_presence[22]		= true;             
+OCLCollimator_presence[22]	= true;             
+OCLLaBr3_Distance[22]		= 20*cm;             
+OCLLaBr3_theta[22]			= 79.187559*deg;             
+OCLLaBr3_phi[22]			= 180.000044*deg;
+
+//Det. number 23             
+OCLLaBr3_presence[23]		= true;             
+OCLCollimator_presence[23]	= true;             
+OCLLaBr3_Distance[23]		= 20*cm;             
+OCLLaBr3_theta[23]			= 63.434885*deg;             
+OCLLaBr3_phi[23]			= 215.999955*deg;
+
+//Det. number 24             
+OCLLaBr3_presence[24]		= true;             
+OCLCollimator_presence[24]	= true;             
+OCLLaBr3_Distance[24]		= 20*cm;             
+OCLLaBr3_theta[24]			= 79.187575*deg;             
+OCLLaBr3_phi[24]			= 251.999954*deg;
+
+//Det. number 25
+OCLLaBr3_presence[25]		= true;             
+OCLCollimator_presence[25]	= true;             
+OCLLaBr3_Distance[25]		= 20*cm;             
+OCLLaBr3_theta[25]			= 63.434949*deg;             
+OCLLaBr3_phi[25]			= 288.000000*deg;
+
+//Det. number 26             
+OCLLaBr3_presence[26]		= true;             
+OCLCollimator_presence[26]	= true;             
+OCLLaBr3_Distance[26]		= 20*cm;             
+OCLLaBr3_theta[26]			= 37.377328*deg;             
+OCLLaBr3_phi[26]			= 324.000134*deg;
+
+//Det. number 27             
+OCLLaBr3_presence[27]		= true;             
+OCLCollimator_presence[27]	= true;             
+OCLLaBr3_Distance[27]		= 20*cm;             
+OCLLaBr3_theta[27]			= 37.377294*deg;             
+OCLLaBr3_phi[27]			= 36.000153*deg;
 
 //Det. number 28           
 OCLLaBr3_presence[15]		= true;             
@@ -356,117 +447,27 @@ OCLLaBr3_Distance[15]		= 20*cm;
 OCLLaBr3_theta[15]			= 37.377321*deg;             
 OCLLaBr3_phi[15]			= 108.000000*deg;
  
-//Det. number 27             
-OCLLaBr3_presence[16]		= true;             
-OCLCollimator_presence[16]	= true;             
-OCLLaBr3_Distance[16]		= 20*cm;             
-OCLLaBr3_theta[16]			= 37.377294*deg;             
-OCLLaBr3_phi[16]			= 36.000153*deg;
-
-//Det. number 26             
-OCLLaBr3_presence[17]		= true;             
-OCLCollimator_presence[17]	= true;             
-OCLLaBr3_Distance[17]		= 20*cm;             
-OCLLaBr3_theta[17]			= 37.377328*deg;             
-OCLLaBr3_phi[17]			= 324.000134*deg;
- 
-//Det. number 30             
-OCLLaBr3_presence[18]		= true;             
-OCLCollimator_presence[18]	= true;             
-OCLLaBr3_Distance[18]		= 20*cm;             
-OCLLaBr3_theta[18]			= 37.377328*deg;             
-OCLLaBr3_phi[18]			= 251.999866*deg;
-
 //Det. number 29             
-OCLLaBr3_presence[19]		= true;             
-OCLCollimator_presence[19]	= true;             
-OCLLaBr3_Distance[19]		= 20*cm;             
-OCLLaBr3_theta[19]			= 37.377294*deg;             
-OCLLaBr3_phi[19]			= 179.999847*deg;
- 
-//Beamline            
-OCLLaBr3_presence[20]		= false;             
-OCLCollimator_presence[20]	= false;             
-OCLLaBr3_Distance[20]		= 20*cm;             
-OCLLaBr3_theta[20]			= 180.000000*deg;             
-OCLLaBr3_phi[20]			= 180.000000*deg;
-
-//Det. number 10             
-OCLLaBr3_presence[21]		= true;             
-OCLCollimator_presence[21]	= true;             
-OCLLaBr3_Distance[21]		= 20*cm;             
-OCLLaBr3_theta[21]			= 116.564844*deg;             
-OCLLaBr3_phi[21]			= 108.000000*deg;
-
-//Det. number 12             
-OCLLaBr3_presence[22]		= true;             
-OCLCollimator_presence[22]	= true;             
-OCLLaBr3_Distance[22]		= 20*cm;             
-OCLLaBr3_theta[22]			= 116.564908*deg;             
-OCLLaBr3_phi[22]			= 180.000045*deg;
-
-//Det. number 14             
-OCLLaBr3_presence[23]		= true;             
-OCLCollimator_presence[23]	= true;             
-OCLLaBr3_Distance[23]		= 20*cm;             
-OCLLaBr3_theta[23]			= 116.564893*deg;             
-OCLLaBr3_phi[23]			= 252.000011*deg;
-  
-//Det. number 6           
-OCLLaBr3_presence[24]		= true;             
-OCLCollimator_presence[24]	= true;             
-OCLLaBr3_Distance[24]		= 20*cm;             
-OCLLaBr3_theta[24]			= 116.564893*deg;             
-OCLLaBr3_phi[24]			= 323.999989*deg;
-  
-//Det. number 8           
-OCLLaBr3_presence[25]		= true;             
-OCLCollimator_presence[25]	= true;             
-OCLLaBr3_Distance[25]		= 20*cm;             
-OCLLaBr3_theta[25]			= 116.564908*deg;             
-OCLLaBr3_phi[25]			= 35.999955*deg;
-
-//Beamline             
-OCLLaBr3_presence[26]		= false;             
-OCLCollimator_presence[26]	= false;             
-OCLLaBr3_Distance[26]		= 20*cm;             
-OCLLaBr3_theta[26]			= 0.000000*deg;             
-OCLLaBr3_phi[26]			= 0.000000*deg;
-
-//Det. number 19             
-OCLLaBr3_presence[27]		= true;             
-OCLCollimator_presence[27]	= true;             
-OCLLaBr3_Distance[27]		= 20*cm;             
-OCLLaBr3_theta[27]			= 63.434900*deg;             
-OCLLaBr3_phi[27]			= 72.000011*deg;
-
-//Det. number 17             
-OCLLaBr3_presence[28]		= true;             
-OCLCollimator_presence[28]	= true;             
-OCLLaBr3_Distance[28]		= 20*cm;             
-OCLLaBr3_theta[28]			= 63.434885*deg;             
-OCLLaBr3_phi[28]			= 0.000045*deg;
-             
-//Det. number 25
 OCLLaBr3_presence[29]		= true;             
 OCLCollimator_presence[29]	= true;             
 OCLLaBr3_Distance[29]		= 20*cm;             
-OCLLaBr3_theta[29]			= 63.434949*deg;             
-OCLLaBr3_phi[29]			= 288.000000*deg;
+OCLLaBr3_theta[29]			= 37.377294*deg;             
+OCLLaBr3_phi[29]			= 179.999847*deg;
 
-//Det. number 23             
+//Det. number 30             
 OCLLaBr3_presence[30]		= true;             
 OCLCollimator_presence[30]	= true;             
 OCLLaBr3_Distance[30]		= 20*cm;             
-OCLLaBr3_theta[30]			= 63.434885*deg;             
-OCLLaBr3_phi[30]			= 215.999955*deg;
+OCLLaBr3_theta[30]			= 37.377328*deg;             
+OCLLaBr3_phi[30]			= 251.999866*deg;
 
-//Det. number 21             
-OCLLaBr3_presence[31]		= true;             
-OCLCollimator_presence[31]	= true;             
+//Beamline             
+OCLLaBr3_presence[31]		= false;             
+OCLCollimator_presence[31]	= false;             
 OCLLaBr3_Distance[31]		= 20*cm;             
-OCLLaBr3_theta[31]			= 63.434900*deg;             
-OCLLaBr3_phi[31]			= 143.999989*deg;
+OCLLaBr3_theta[31]			= 0.000000*deg;             
+OCLLaBr3_phi[31]			= 0.000000*deg;
+
 
 G4double disttoLaBr3Half;
 G4double disttoCollHalf;

commit 5005bcb6d7ba7e86b1023a48649dc6d61250340f

Update minor mistake in detector numbering
Updated small mistake introduced in
f72c738d9332c5ae5e69bd4e8ec9c773d860c09e. Detector number 28 should have array number 28, not 15.
The numbering is now like in the lab (checked detectors 26-30).

Cheers,

Fabio

SingleScint/src/SingleScintDetectorConstruction.cc
@@ -441,11 +441,11 @@ OCLLaBr3_theta[27]			= 37.377294*deg;
 OCLLaBr3_phi[27]			= 36.000153*deg;
 
 //Det. number 28           
-OCLLaBr3_presence[15]		= true;             
-OCLCollimator_presence[15]	= true;             
-OCLLaBr3_Distance[15]		= 20*cm;             
-OCLLaBr3_theta[15]			= 37.377321*deg;             
-OCLLaBr3_phi[15]			= 108.000000*deg;
+OCLLaBr3_presence[28]		= true;             
+OCLCollimator_presence[28]	= true;             
+OCLLaBr3_Distance[28]		= 20*cm;             
+OCLLaBr3_theta[28]			= 37.377321*deg;             
+OCLLaBr3_phi[28]			= 108.000000*deg;
  
 //Det. number 29             
 OCLLaBr3_presence[29]		= true;             