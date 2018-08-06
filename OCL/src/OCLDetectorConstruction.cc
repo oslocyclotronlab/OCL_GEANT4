bde83638e9d68a40985143074ba3d87d1e2af3e1
fzeiser <fzeiser@physnet.uni-hamburg.de>
2018-08-06 13:35:52 +0200

    Corrected det. distance for hexagons vs pentagons
    In the football geometry, hexagons and pentagons have a different face-source distances. If the same spacers are used, this results in about 7mm different of the distances of pentagons and hexagons.

--- OCL/src/OCLDetectorConstruction.cc
+++ OCL/src/OCLDetectorConstruction.cc
@@ -256,217 +256,217 @@ G4double distColltoDet = 10*mm; // Distance between Collimator and Detector (sur
 //Beamline            
 OCLLaBr3_presence[0]		= false;             
 OCLCollimator_presence[0]	= false;             
-OCLLaBr3_Distance[0]		= 20*cm;             
+OCLLaBr3_Distance[0]		= 20.*cm;             
 OCLLaBr3_theta[00]			= 180.000000*deg;             
 OCLLaBr3_phi[0]			= 180.000000*deg;
 
 //Det. number 1 at OCL:            
 OCLLaBr3_presence[ 1]		= true;             
 OCLCollimator_presence[ 1]	= true;             
-OCLLaBr3_Distance[ 1]		= 20*cm;             
+OCLLaBr3_Distance[ 1]		= 24.2*cm;             
 OCLLaBr3_theta[ 1]			= 142.622561*deg;             
 OCLLaBr3_phi[ 1]			= 359.999847*deg;
 
 //Det. number 2 at OCL             
 OCLLaBr3_presence[ 2]		= true;             
 OCLCollimator_presence[ 2]	= true;             
-OCLLaBr3_Distance[ 2]		= 20*cm;             
+OCLLaBr3_Distance[ 2]		= 24.2*cm;             
 OCLLaBr3_theta[ 2]			= 142.622528*deg;             
 OCLLaBr3_phi[ 2]			= 71.999866*deg;
 
 //Det. number 3 at OCL:
-OCLLaBr3_presence[ 3]		= true;             
+OCLLaBr3_presence[ 3]		= false;             
 OCLCollimator_presence[ 3]	= true;             
-OCLLaBr3_Distance[ 3]		= 20*cm;             
+OCLLaBr3_Distance[ 3]		= 24.2*cm;             
 OCLLaBr3_theta[ 3]			= 142.622528*deg;             
 OCLLaBr3_phi[ 3]			= 144.000134*deg;
 
 //Det. number 4 at OCL             
-OCLLaBr3_presence[ 4]		= true;             
+OCLLaBr3_presence[ 4]		= false;             
 OCLCollimator_presence[ 4]	= true;             
-OCLLaBr3_Distance[ 4]		= 20*cm;             
+OCLLaBr3_Distance[ 4]		= 24.2*cm;             
 OCLLaBr3_theta[ 4]			= 142.622561*deg;             
 OCLLaBr3_phi[ 4]			= 216.000153*deg;
              
 //Det. number 5 at OCL  
-OCLLaBr3_presence[ 5]		= true;             
+OCLLaBr3_presence[ 5]		= false;             
 OCLCollimator_presence[ 5]	= true;             
-OCLLaBr3_Distance[ 5]		= 20*cm;             
+OCLLaBr3_Distance[ 5]		= 24.2*cm;             
 OCLLaBr3_theta[ 5]			= 142.622535*deg;             
 OCLLaBr3_phi[ 5]			= 288.000000*deg;
 
 //Det. number 6           
 OCLLaBr3_presence[6]		= true;             
 OCLCollimator_presence[6]	= true;             
-OCLLaBr3_Distance[6]		= 20*cm;             
+OCLLaBr3_Distance[6]		= 23.5*cm;             
 OCLLaBr3_theta[6]			= 116.564893*deg;             
 OCLLaBr3_phi[6]				= 323.999989*deg;
 
 //Det. number 7
 OCLLaBr3_presence[ 7]		= true;             
 OCLCollimator_presence[ 7]	= true;             
-OCLLaBr3_Distance[ 7]		= 20*cm;             
+OCLLaBr3_Distance[ 7]		= 24.2*cm;             
 OCLLaBr3_theta[ 7]			= 100.812208*deg;             
 OCLLaBr3_phi[ 7]			= 0.000044*deg;
 
 //Det. number 8           
 OCLLaBr3_presence[ 8]		= true;             
 OCLCollimator_presence[ 8]	= true;             
-OCLLaBr3_Distance[ 8]		= 20*cm;             
+OCLLaBr3_Distance[ 8]		= 23.5*cm;             
 OCLLaBr3_theta[ 8]			= 116.564908*deg;             
 OCLLaBr3_phi[ 8]			= 35.999955*deg;
 
 //Det. number 9 at OCL:             
 OCLLaBr3_presence[ 9]		= true;             
 OCLCollimator_presence[ 9]	= true;             
-OCLLaBr3_Distance[ 9]		= 20*cm;             
+OCLLaBr3_Distance[ 9]		= 24.2*cm;             
 OCLLaBr3_theta[ 9]			= 100.812191*deg;             
 OCLLaBr3_phi[ 9]			= 71.999954*deg;
 
 //Det. number 10             
 OCLLaBr3_presence[10]		= true;             
 OCLCollimator_presence[10]	= true;             
-OCLLaBr3_Distance[10]		= 20*cm;             
+OCLLaBr3_Distance[10]		= 23.5*cm;             
 OCLLaBr3_theta[10]			= 116.564844*deg;             
 OCLLaBr3_phi[10]			= 108.000000*deg;
 
 //Det. number 11        
 OCLLaBr3_presence[11]		= true;             
 OCLCollimator_presence[11]	= true;             
-OCLLaBr3_Distance[11]		= 20*cm;             
+OCLLaBr3_Distance[11]		= 24.2*cm;             
 OCLLaBr3_theta[11]			= 100.812191*deg;             
 OCLLaBr3_phi[11]			= 144.000046*deg;
 
 //Det. number 12             
 OCLLaBr3_presence[12]		= true;             
 OCLCollimator_presence[12]	= true;             
-OCLLaBr3_Distance[12]		= 20*cm;             
+OCLLaBr3_Distance[12]		= 23.5*cm;             
 OCLLaBr3_theta[12]			= 116.564908*deg;             
 OCLLaBr3_phi[12]			= 180.000045*deg;
 
 //Det. number 13:             
 OCLLaBr3_presence[13]		= true;             
 OCLCollimator_presence[13]	= true;             
-OCLLaBr3_Distance[13]		= 20*cm;             
+OCLLaBr3_Distance[13]		= 24.2*cm;             
 OCLLaBr3_theta[13]			= 100.812208*deg;             
 OCLLaBr3_phi[13]			= 215.999956*deg;
 
 //Det. number 14             
 OCLLaBr3_presence[14]		= true;             
 OCLCollimator_presence[14]	= true;             
-OCLLaBr3_Distance[14]		= 20*cm;             
+OCLLaBr3_Distance[14]		= 23.5*cm;             
 OCLLaBr3_theta[14]			= 116.564893*deg;             
 OCLLaBr3_phi[14]			= 252.000011*deg;
 
 //Det. number 15             
 OCLLaBr3_presence[15]		= true;             
 OCLCollimator_presence[15]	= true;             
-OCLLaBr3_Distance[15]		= 20*cm;             
+OCLLaBr3_Distance[15]		= 24.2*cm;             
 OCLLaBr3_theta[15]			= 100.812175*deg;             
 OCLLaBr3_phi[15]			= 288.000000*deg;
 
 //Det. number 16           
 OCLLaBr3_presence[16]		= true;             
 OCLCollimator_presence[16]	= true;             
-OCLLaBr3_Distance[16]		= 20*cm;             
+OCLLaBr3_Distance[16]		= 24.2*cm;             
 OCLLaBr3_theta[16]			= 79.187575*deg;             
 OCLLaBr3_phi[16]			= 324.000046*deg;
 
 //Det. number 17             
 OCLLaBr3_presence[17]		= true;             
 OCLCollimator_presence[17]	= true;             
-OCLLaBr3_Distance[17]		= 20*cm;             
+OCLLaBr3_Distance[17]		= 23.5*cm;             
 OCLLaBr3_theta[17]			= 63.434885*deg;             
 OCLLaBr3_phi[17]			= 0.000045*deg;
 
 //Det. number 18      
-OCLLaBr3_presence[18]		= true;             
+OCLLaBr3_presence[18]		= false;             
 OCLCollimator_presence[18]	= true;             
-OCLLaBr3_Distance[18]		= 20*cm;             
+OCLLaBr3_Distance[18]		= 23.5*cm;             
 OCLLaBr3_theta[18]			= 79.187559*deg;             
 OCLLaBr3_phi[18]			= 35.999956*deg;
 
 //Det. number 19             
 OCLLaBr3_presence[19]		= true;             
 OCLCollimator_presence[19]	= true;             
-OCLLaBr3_Distance[19]		= 20*cm;             
+OCLLaBr3_Distance[19]		= 24.2*cm;             
 OCLLaBr3_theta[19]			= 63.434900*deg;             
 OCLLaBr3_phi[19]			= 72.000011*deg;
 
 //Det. number 20             
 OCLLaBr3_presence[20]		= true;             
 OCLCollimator_presence[20]	= true;             
-OCLLaBr3_Distance[20]		= 20*cm;             
+OCLLaBr3_Distance[20]		= 23.5*cm;             
 OCLLaBr3_theta[20]			= 79.187591*deg;             
 OCLLaBr3_phi[20]			= 108.000000*deg;
 
 //Det. number 21             
-OCLLaBr3_presence[21]		= true;             
+OCLLaBr3_presence[21]		= false;             
 OCLCollimator_presence[21]	= true;             
-OCLLaBr3_Distance[21]		= 20*cm;             
+OCLLaBr3_Distance[21]		= 23.5*cm;             
 OCLLaBr3_theta[21]			= 63.434900*deg;             
 OCLLaBr3_phi[21]			= 143.999989*deg;
 
 //Det. number 22             
 OCLLaBr3_presence[22]		= true;             
 OCLCollimator_presence[22]	= true;             
-OCLLaBr3_Distance[22]		= 20*cm;             
+OCLLaBr3_Distance[22]		= 23.5*cm;             
 OCLLaBr3_theta[22]			= 79.187559*deg;             
 OCLLaBr3_phi[22]			= 180.000044*deg;
 
 //Det. number 23             
-OCLLaBr3_presence[23]		= true;             
+OCLLaBr3_presence[23]		= false;             
 OCLCollimator_presence[23]	= true;             
-OCLLaBr3_Distance[23]		= 20*cm;             
+OCLLaBr3_Distance[23]		= 24.2*cm;             
 OCLLaBr3_theta[23]			= 63.434885*deg;             
 OCLLaBr3_phi[23]			= 215.999955*deg;
 
 //Det. number 24             
 OCLLaBr3_presence[24]		= true;             
 OCLCollimator_presence[24]	= true;             
-OCLLaBr3_Distance[24]		= 20*cm;             
+OCLLaBr3_Distance[24]		= 24.2*cm;             
 OCLLaBr3_theta[24]			= 79.187575*deg;             
 OCLLaBr3_phi[24]			= 251.999954*deg;
 
 //Det. number 25
 OCLLaBr3_presence[25]		= true;             
 OCLCollimator_presence[25]	= true;             
-OCLLaBr3_Distance[25]		= 20*cm;             
+OCLLaBr3_Distance[25]		= 24.2*cm;             
 OCLLaBr3_theta[25]			= 63.434949*deg;             
 OCLLaBr3_phi[25]			= 288.000000*deg;
 
 //Det. number 26             
-OCLLaBr3_presence[26]		= true;             
+OCLLaBr3_presence[26]		= false;             
 OCLCollimator_presence[26]	= true;             
-OCLLaBr3_Distance[26]		= 20*cm;             
+OCLLaBr3_Distance[26]		= 24.2*cm;             
 OCLLaBr3_theta[26]			= 37.377328*deg;             
 OCLLaBr3_phi[26]			= 324.000134*deg;
 
 //Det. number 27             
 OCLLaBr3_presence[27]		= true;             
 OCLCollimator_presence[27]	= true;             
-OCLLaBr3_Distance[27]		= 20*cm;             
+OCLLaBr3_Distance[27]		= 24.2*cm;             
 OCLLaBr3_theta[27]			= 37.377294*deg;             
 OCLLaBr3_phi[27]			= 36.000153*deg;
 
 //Det. number 28           
 OCLLaBr3_presence[28]		= true;             
 OCLCollimator_presence[28]	= true;             
-OCLLaBr3_Distance[28]		= 20*cm;             
+OCLLaBr3_Distance[28]		= 24.2*cm;             
 OCLLaBr3_theta[28]			= 37.377321*deg;             
 OCLLaBr3_phi[28]			= 108.000000*deg;
  
 //Det. number 29             
-OCLLaBr3_presence[29]		= true;             
+OCLLaBr3_presence[29]		= false;             
 OCLCollimator_presence[29]	= true;             
-OCLLaBr3_Distance[29]		= 20*cm;             
+OCLLaBr3_Distance[29]		= 24.2*cm;             
 OCLLaBr3_theta[29]			= 37.377294*deg;             
 OCLLaBr3_phi[29]			= 179.999847*deg;
 
 //Det. number 30             
-OCLLaBr3_presence[30]		= true;             
+OCLLaBr3_presence[30]		= false;             
 OCLCollimator_presence[30]	= true;             
-OCLLaBr3_Distance[30]		= 20*cm;             
+OCLLaBr3_Distance[30]		= 24.2*cm;             
 OCLLaBr3_theta[30]			= 37.377328*deg;             
 OCLLaBr3_phi[30]			= 251.999866*deg;
 
@@ -485,7 +485,6 @@ G4double offsettoCollimator = distColltoDet + collimatorHalfLength;
 
 for(G4int i=0; i<numberOf_OCLLaBr3; i++){
 	
-
 	disttoLaBr3Half = OCLLaBr3_Distance[i] 
 					  + detectorHalfinclPMT;
 	disttoCollHalf =  OCLLaBr3_Distance[i] - offsettoCollimator;
